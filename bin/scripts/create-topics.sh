1e79e9406406745f2e5dadf051663b0c82f4a0ff
Harshik Patel <harshikpatel2710@gmail.com>
2024-06-05 12:20:57 -0400

    Initial commit

---
+++ bin/scripts/create-topics.sh
@@ -0,0 +1,19 @@
+
+cub kafka-ready -b kafka:9092 1 20
+
+# create the users topic
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic sentences \
+  --replication-factor 1 \
+  --partitions 1 \
+  --create
+
+kafka-topics \
+  --bootstrap-server kafka:9092 \
+  --topic word-count \
+  --replication-factor 1 \
+  --partitions 1 \
+  --create
+
+sleep infinity
---
+++ src/main/java/org/kafka/codewithjalo/word_count/App.java
@@ -0,0 +1,13 @@
+package org.kafka.codewithjalo.word_count;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
